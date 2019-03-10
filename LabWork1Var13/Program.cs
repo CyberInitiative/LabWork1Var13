b4bf7c440033920da1e4c4ede11395fc371bc3f9
Мирослав Левдиков <deadstronghold@gmail.com>
2019-03-10 19:30:53 +0200

    Добавьте файлы проекта.

---
+++ LabWork1Var13/Program.cs
@@ -0,0 +1,32 @@
+﻿// Вариант 13. Реализовать функцию возведения в квадрат частного двух целых чисел.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Program method = new Program();
+            Console.WriteLine("Введите число №1");
+            string s = Console.ReadLine();
+            int a = Convert.ToInt32(s);
+            Console.WriteLine("Введите число №2");
+            string r = Console.ReadLine();
+            int b = Convert.ToInt32(r);
+            if (b == 0)
+                Environment.Exit(0);
+            Console.WriteLine("Квадрат частного двух целых чисел: " + method.LabWorkMethod(a, b));
+            Console.ReadLine();
+        }
+        public int LabWorkMethod(int a, int b)
+        {
+            return ((a / b) * (a / b));
+        }
+    }
+}
